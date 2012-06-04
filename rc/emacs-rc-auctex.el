171a4b2b7f9a062b98d57ab5afda1971c5f29228
Dmitry Groshev <groshev@selectel.ru>
2012-06-04 18:20:02 +0400

    move xelatex adoption to hook

--- rc/emacs-rc-auctex.el
+++ rc/emacs-rc-auctex.el
@@ -6,13 +6,14 @@
  TeX-parse-self t
  TeX-DVI-via-PDFTeX t)
 
-(add-to-list 'TeX-command-list
-             '("XeLaTeX" "xelatex -interaction=nonstopmode -shell-escape %s"
-               TeX-run-command t t :help "Run xelatex") t)
+
 
 (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
 (add-hook 'LaTeX-mode-hook
           '(lambda ()
              (TeX-fold-mode 1)
              (TeX-PDF-mode 1)
-             (outline-minor-mode 1)))
+             (outline-minor-mode 1)
+             (add-to-list 'TeX-command-list
+                          '("XeLaTeX" "xelatex -interaction=nonstopmode -shell-escape %s"
+                            TeX-run-command t t :help "Run xelatex") t)))
