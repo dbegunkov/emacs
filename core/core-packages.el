b67d68ebecd77bbc253b6ae2f3820193a0bff625
dbegunkov <dbegunkov@smprc.ru>
2016-07-11 09:31:05 +0300

    fix anacone mode

--- core/core-packages.el
+++ core/core-packages.el
@@ -7,6 +7,10 @@
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (add-to-list 'package-archives
              '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/") t)
+(add-to-list 'package-archives
+             '("gnu" . "http://elpa.gnu.org/packages/") t)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
 ;; (add-to-list 'package-archives
 ;;              '("marmalade" . "http://marmalade-repo.org/packages/") t)
 
